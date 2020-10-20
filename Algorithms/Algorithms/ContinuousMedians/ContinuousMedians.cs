78861753e7b86d0dfd21eb2ccafdf22469247a50
Ciaran ODonnell <wannabedeveloper@live.com>
2020-10-20 09:58:16 -0400

    Solve for Continuous Means

---
+++ Algorithms/Algorithms/ContinuousMedians/ContinuousMedians.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Algorithms
+{
+	class ContinuousMedians
+	{
+
+
+		public static void MyMain(string[] args)
+		{
+			ContinuousMedians m = new ContinuousMedians();
+			int continousMedian = 0;
+			for (int x = 0; x < 100; x++)
+			{
+				var i = int.Parse(Console.ReadLine());
+				var median = m.Add(i);
+				continousMedian += median;
+				Console.Write(continousMedian);
+				Console.WriteLine("  (" + median + " | " + string.Join(' ', m.values) + ")");
+			}
+
+		}
+
+
+
+		LinkedList<int> values = new LinkedList<int>();
+		bool first = true;
+		private LinkedListNode<int> medianNode;
+		bool moveMedian = false;
+		public int Add(int value)
+		{
+			int newMedian = 0;
+
+			if (first)
+			{
+				first = false;
+				this.medianNode = values.AddLast(value);
+				return value;
+			}
+
+			var insertedBeforeMedian = InsertInPlace(value);
+			if (insertedBeforeMedian && moveMedian == false) medianNode = medianNode.Previous;
+			if (moveMedian)
+			{
+				if (!insertedBeforeMedian) medianNode = medianNode.Next;
+				moveMedian = false;
+				return medianNode.Value;
+			}
+			else
+			{
+				moveMedian = true;
+				return ((medianNode.Value + medianNode.Next.Value) / 2);
+				
+			}
+
+
+		}
+
+		private bool InsertInPlace(int value)
+		{
+			if (value >= values.Last.Value)
+			{
+				values.AddLast(value);
+				return false;
+			}
+			if (value <= values.First.Value)
+			{
+				values.AddFirst(value);
+				return true;
+			}
+
+			if (value <= medianNode.Value)
+			{
+				var node = medianNode.Previous;
+				while (value <= node.Value) node = node.Previous;
+				values.AddAfter(node, value);
+				return true;
+			}
+			else
+			{
+				var node = medianNode.Next;
+				while (value >= node.Value) node = node.Next;
+				values.AddBefore(node, value);
+				return false;
+			}
+
+
+		}
+	}
+}
